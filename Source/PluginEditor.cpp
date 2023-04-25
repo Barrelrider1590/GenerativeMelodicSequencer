79433b09853d2757e2996143067cd53acf8b0434
Barrelrider1590 <dragonage1590@gmail.com>
2023-04-25 18:04:51 +0200

    Changed Window
    Changed:
    - Window size
    - Window text
    - Window background colour

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -15,7 +15,7 @@ GenerativeMelodicSequencerAudioProcessorEditor::GenerativeMelodicSequencerAudioP
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    setSize (350, 600);
 }
 
 GenerativeMelodicSequencerAudioProcessorEditor::~GenerativeMelodicSequencerAudioProcessorEditor()
@@ -26,11 +26,11 @@ GenerativeMelodicSequencerAudioProcessorEditor::~GenerativeMelodicSequencerAudio
 void GenerativeMelodicSequencerAudioProcessorEditor::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+    g.fillAll (juce::Colour (20,20,20));
 
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.drawFittedText ("Generative Melodic Squencer", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void GenerativeMelodicSequencerAudioProcessorEditor::resized()
