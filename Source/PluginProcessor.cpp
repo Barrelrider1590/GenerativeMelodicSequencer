4fd71842cd8798f92fec5c9f3a5f025cc545c5e6
Barrelrider1590 <dragonage1590@gmail.com>
2023-04-27 10:06:53 +0200

    Generating MIDI
    Added a function that populates a midi buffer and adding the buffer events to the processBlock buffer

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -19,7 +19,8 @@ GenerativeMelodicSequencerAudioProcessor::GenerativeMelodicSequencerAudioProcess
                       #endif
                        .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
                      #endif
-                       )
+                       ),
+    m_midiChannel(10), m_startTime(juce::Time::getMillisecondCounterHiRes() * .001)
 #endif
 {
 }
@@ -93,8 +94,10 @@ void GenerativeMelodicSequencerAudioProcessor::changeProgramName (int index, con
 //==============================================================================
 void GenerativeMelodicSequencerAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
 {
-    // Use this method as the place to do any pre-playback
-    // initialisation that you need..
+    auto currentTime = juce::Time::getMillisecondCounterHiRes() * 0.001 - m_startTime;
+    auto currentSampleNumber = (int)(currentTime * sampleRate);
+
+    populateMidiBuffer(m_midiBuffer, m_midiChannel, currentSampleNumber);
 }
 
 void GenerativeMelodicSequencerAudioProcessor::releaseResources()
@@ -132,6 +135,7 @@ bool GenerativeMelodicSequencerAudioProcessor::isBusesLayoutSupported (const Bus
 void GenerativeMelodicSequencerAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
 {
 
+    
 }
 
 //==============================================================================
@@ -191,4 +195,14 @@ juce::AudioProcessorValueTreeState::ParameterLayout GenerativeMelodicSequencerAu
     layout.add(std::make_unique < juce::AudioParameterFloat>("mutate", "Mutate", 0.f, 1.f, .5f));
 
     return layout;
+}
+
+void GenerativeMelodicSequencerAudioProcessor::populateMidiBuffer(juce::MidiBuffer& midiBuffer, int midiChannel, int sampleNr)
+{
+    for (int i = 0; i < 2; ++i)
+    {
+        auto timeStamp{ juce::Time::getMillisecondCounterHiRes };
+
+        midiBuffer.addEvent(juce::MidiMessage::noteOn(midiChannel, 36, (juce::uint8)127), sampleNr + (i * 500));
+    }
 }
