82cc9afcb2126e5baf7003a0c4c4fdc679717dbc
ValentijnVS <53330307+Barrelrider1590@users.noreply.github.com>
2023-05-07 14:37:22 +0200

    Revert "Set project to take midi input"
    This reverts commit 238139bcc45e10bf876c5be93e09d1608e748525.

--- JuceLibraryCode/JucePluginDefines.h
+++ JuceLibraryCode/JucePluginDefines.h
@@ -65,7 +65,7 @@
  #define JucePlugin_IsSynth                1
 #endif
 #ifndef  JucePlugin_WantsMidiInput
- #define JucePlugin_WantsMidiInput         1
+ #define JucePlugin_WantsMidiInput         0
 #endif
 #ifndef  JucePlugin_ProducesMidiOutput
  #define JucePlugin_ProducesMidiOutput     0
@@ -131,7 +131,7 @@
  #define JucePlugin_AAXDisableMultiMono    0
 #endif
 #ifndef  JucePlugin_IAAType
- #define JucePlugin_IAAType                0x61757269
+ #define JucePlugin_IAAType                0x61757267
 #endif
 #ifndef  JucePlugin_IAASubType
  #define JucePlugin_IAASubType             JucePlugin_PluginCode
