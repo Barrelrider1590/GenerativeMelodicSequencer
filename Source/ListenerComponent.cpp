1930a6ffc3bc2b5b85cfc000eb4f38d23bfa5df5
ValentijnVS <53330307+Barrelrider1590@users.noreply.github.com>
2023-06-05 14:55:41 +0200

    Revert "Revert "Add: BroadcasterComponent and ListenerComponent""
    This reverts commit d7e0aa44264fba00d8e0bdd19b2f232da6cba11d.

---
+++ Source/ListenerComponent.cpp
@@ -0,0 +1,27 @@
+/*
+  ==============================================================================
+
+    ListenerComponent.cpp
+    Created: 5 Jun 2023 2:14:46pm
+    Author:  Phoenix
+
+  ==============================================================================
+*/
+
+#include "ListenerComponent.h"
+ListenerComponent::ListenerComponent()
+{
+
+}
+void ListenerComponent::paint(juce::Graphics& g)
+{
+
+}
+void ListenerComponent::resized()
+{
+
+}
+void ListenerComponent::changeListenerCallback(juce::ChangeBroadcaster* source)
+{
+
+}
