b5e874efe91a793ccb6c7c5f9caf53bc0f76e756
ValentijnVS <53330307+Barrelrider1590@users.noreply.github.com>
2023-06-07 17:10:37 +0200

    Refactor: CustomLookAndFeel
    Gradient now a private member variable that is initialised in the constructor
    Created colour variables inside the drawRotarySlider function, a base and an accent colour

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -36,8 +36,15 @@ struct Vector2f
 class CustomLookAndFeel : public juce::LookAndFeel_V4
 {
 public:
-    CustomLookAndFeel(){}
-
+    CustomLookAndFeel()
+    {
+        m_gradient.addColour(0, juce::Colours::rebeccapurple);
+        m_gradient.addColour(.25, juce::Colours::blueviolet);
+        m_gradient.addColour(.5, juce::Colours::deeppink);
+        m_gradient.addColour(.75, juce::Colours::darkorange);
+        m_gradient.addColour(1, juce::Colours::goldenrod);
+    }
+    //==============================================================================
     void drawRotarySlider(juce::Graphics& g,
         int x, int y,
         int width, int height,
@@ -50,49 +57,52 @@ public:
         Vector2f ellipseCentre{ containerCentre.x - radius * .5f, containerCentre.y - radius * .5f};
 
         float angle{ rotaryStartAngle + sliderPos * (rotaryEndAngle - rotaryStartAngle) };
-
-        juce::ColourGradient myBeautifulGradient;
-        myBeautifulGradient.addColour(0, juce::Colours::rebeccapurple);
-        myBeautifulGradient.addColour(.25, juce::Colours::blueviolet);
-        myBeautifulGradient.addColour(.5, juce::Colours::deeppink);
-        myBeautifulGradient.addColour(.75, juce::Colours::darkorange);
-        myBeautifulGradient.addColour(1, juce::Colours::goldenrod);
+        
+        juce::Colour knobBaseClr{ juce::Colour::Colour(100, 100, 100) };
+        juce::Colour knobAccentClr{ m_gradient.getColourAtPosition((angle - rotaryStartAngle) / (rotaryEndAngle - rotaryStartAngle)) };
 
         // drawing knob
-        g.setColour(juce::Colour::Colour(100, 100, 100));
+        g.setColour(knobBaseClr);
         g.fillEllipse(ellipseCentre.x, ellipseCentre.y, radius, radius);
 
-        g.setColour(myBeautifulGradient.getColourAtPosition((angle - rotaryStartAngle) / (rotaryEndAngle - rotaryStartAngle)));
+        g.setColour(knobAccentClr);
         g.drawEllipse(ellipseCentre.x, ellipseCentre.y, radius, radius, 5.f);
 
         // drawing pointer
-        juce::Path p{};
+        juce::PathStrokeType strokeType{ juce::PathStrokeType::PathStrokeType(radius * .05f,
+                                              juce::PathStrokeType::JointStyle::curved,
+                                              juce::PathStrokeType::EndCapStyle::rounded) };
+
+        juce::Path pointer{};
         float pointerLength{ radius * .25f };
         float pointerThickness{ 10.f };
-        p.addRoundedRectangle(-pointerThickness, -radius * .5f, pointerThickness, pointerLength, 5.f);
-        p.applyTransform(juce::AffineTransform::rotation(angle).scaled(1.2f).translated(containerCentre.x, containerCentre.y));
 
-        g.setColour(myBeautifulGradient.getColourAtPosition((angle - rotaryStartAngle) / (rotaryEndAngle - rotaryStartAngle)));
-        g.fillPath(p);
+        pointer.addRoundedRectangle(-pointerThickness, -radius * .5f, pointerThickness, pointerLength, 5.f);
+        pointer.applyTransform(juce::AffineTransform::rotation(angle).scaled(1.2f).translated(containerCentre.x, containerCentre.y));
 
-        juce::Path p2{};
-        p2.addCentredArc(containerCentre.x, containerCentre.y,
+        g.setColour(knobBaseClr);
+        g.fillPath(pointer);
+        g.setColour(knobAccentClr);
+        g.strokePath(pointer, strokeType);
+
+        // drawing arc around knob
+        juce::Path pArc{};
+        juce::Path pDashed{};
+        const float dashLengths[2] = { 10.f, 15.f };
+
+        pArc.addCentredArc(containerCentre.x, containerCentre.y,
             radius * .75f, radius * .75f,
             0.f,
             rotaryStartAngle, rotaryEndAngle,
             true);
+        strokeType.createDashedStroke(pDashed, pArc, dashLengths, 2);
 
-        juce::PathStrokeType strokeType{ juce::PathStrokeType::PathStrokeType(radius * .05f,
-                                                              juce::PathStrokeType::JointStyle::curved,
-                                                              juce::PathStrokeType::EndCapStyle::rounded) };
-
-        
-        juce::Path pDashed{};
-        const float dashLengths[2] = { 10.f, 15.f };
-        strokeType.createDashedStroke(pDashed, p2, dashLengths, 2);
-        g.setColour(myBeautifulGradient.getColourAtPosition((angle - rotaryStartAngle) / (rotaryEndAngle - rotaryStartAngle)));
+        g.setColour(knobAccentClr);
         g.strokePath(pDashed, strokeType);
     }
+
+private:
+    juce::ColourGradient m_gradient;
 };
 //==============================================================================
 /**
