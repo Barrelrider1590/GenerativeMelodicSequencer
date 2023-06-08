2c14bc0d308950edbf0e7530e2a12506dafcd7ca
ValentijnVS <53330307+Barrelrider1590@users.noreply.github.com>
2023-06-08 13:39:24 +0200

    Refactored NoteVisualiser
    Now takes background colour as parameter
    changed m_rect to m_bounds

--- Source/NoteVisualiser.h
+++ Source/NoteVisualiser.h
@@ -13,16 +13,16 @@
 
 struct NoteVisual
 {
-    NoteVisual() {}
+    NoteVisual() : colour(juce::Colours::black) {}
     //==============================================================================
-    juce::Colour colour{ juce::Colours::red };
+    juce::Colour colour;
     juce::Rectangle<int> rect;
 };
 
 class NoteVisualiser : public juce::Component
 {
 public:
-    NoteVisualiser()
+    NoteVisualiser(juce::Colour bgClr) : m_backgroundClr(bgClr)
     {
         for (int i{}; i < 12; ++i)
         {
@@ -34,15 +34,15 @@ public:
     //==============================================================================
     void setBounds(juce::Rectangle<int> newBounds)
     {
-        m_rect = newBounds;
-        auto noteBounds = m_rect;
-        noteBounds.setWidth(m_rect.getWidth() / 12);
-        noteBounds.setHeight(m_rect.getHeight() / 12);
+        m_bounds = newBounds;
+        auto noteBounds = m_bounds;
+        noteBounds.setWidth(m_bounds.getWidth() / 12);
+        noteBounds.setHeight(m_bounds.getHeight() / 12);
         int counter{};
         for (auto& note : m_notes)
         {
             noteBounds.setX(noteBounds.getWidth() * counter);
-            noteBounds.setY((m_rect.getHeight() - noteBounds.getHeight()) - (noteBounds.getHeight() * counter));
+            noteBounds.setY((m_bounds.getHeight() - noteBounds.getHeight()) - (noteBounds.getHeight() * counter));
             note.rect = noteBounds;
             ++counter;
         }
@@ -50,13 +50,12 @@ public:
     void paint(juce::Graphics& g) override
     {
         g.setColour(juce::Colour(juce::Colours::black));
-        g.fillRect(m_rect);
-        int counter{};
+        g.fillRect(m_bounds);
+
         for (const auto& note : m_notes)
         {
             g.setColour(note.colour);
             g.fillRect(note.rect);
-            ++counter;
         }
     }
 
@@ -68,6 +67,7 @@ public:
     }
 
 private:
-    juce::Rectangle<int> m_rect;
+    juce::Rectangle<int> m_bounds;
     std::vector<NoteVisual> m_notes;
+    juce::Colour m_backgroundClr;
 };
