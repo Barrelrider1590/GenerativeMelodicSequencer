770987f056ff805be1c563bbc7e1f68c59cf49e1
ValentijnVS <53330307+Barrelrider1590@users.noreply.github.com>
2023-06-16 08:08:47 +0200

    Moved NoteVisual struct to Utility.h

--- Source/NoteVisualiser.h
+++ Source/NoteVisualiser.h
@@ -10,28 +10,7 @@
 
 #pragma once
 #include <JuceHeader.h>
-
-struct NoteVisual
-{
-    NoteVisual() : 
-        m_noteNr(0),
-        m_index(0),
-        m_isActive(false),
-        m_offsetTotal(0),
-        m_colourActive(juce::Colours::green),
-        m_colourInactive(juce::Colours::red),
-        m_rect() {}
-
-    //==============================================================================
-    int m_noteNr;
-    int m_index;
-    bool m_isActive;
-    float m_offsetTotal;
-    juce::Colour m_colourActive;
-    juce::Colour m_colourInactive;
-    juce::Rectangle<int> m_rect;
-};
-
+#include "Utility.h"
 
 class NoteVisualiser : public juce::Component
 {
