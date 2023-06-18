9da7339b48e7ca51ec46a002480b207408500ed5
ValentijnVS <53330307+Barrelrider1590@users.noreply.github.com>
2023-06-18 12:17:14 +0200

    Update NoteVisualiser.cpp
    changed initialiser list order and added new members
    setting maxNoteWidth and height

--- Source/NoteVisualiser.cpp
+++ Source/NoteVisualiser.cpp
@@ -13,15 +13,17 @@
 NoteVisualiser::NoteVisualiser(const juce::ColourGradient& gradient, const juce::Colour& bgClr) :
     m_noteStartPos(0),
     m_nrOfDuplicates(8),
+    m_margin(0.f),
+    m_maxNoteWidth(0.f),
+    m_maxNoteHeight(0.f),
     m_notesVect(),
     m_noteNamesVect({ "c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "b" }),
     m_border(),
     m_bounds(),
-    m_margin(0.f),
     m_gradient(gradient),
     m_backgroundClr(bgClr)
 {
-    int nrOfNotes(14);
+    int nrOfNotes{ 14 };
     m_notesVect.resize(nrOfNotes * m_nrOfDuplicates);
 
     for (int i{}; i < m_notesVect.size(); ++i)
@@ -36,14 +38,16 @@ NoteVisualiser::NoteVisualiser(const juce::ColourGradient& gradient, const juce:
 void NoteVisualiser::setBounds(const juce::Rectangle<int>& newBounds)
 {
     m_border = newBounds;
-
     m_bounds = newBounds;
     m_margin = juce::jmin(m_bounds.getWidth(), m_bounds.getHeight()) * .1f;
     m_bounds.reduce(m_margin, m_margin);
 
     juce::Rectangle<int> noteBounds = m_bounds;
-    noteBounds.setWidth(m_bounds.getWidth() / static_cast<float>(m_notesVect.size() / m_nrOfDuplicates));
-    noteBounds.setHeight(m_bounds.getHeight() / static_cast<float>(m_nrOfDuplicates));
+    m_maxNoteWidth = m_bounds.getWidth() / static_cast<float>(m_notesVect.size() / m_nrOfDuplicates);
+    m_maxNoteHeight = m_bounds.getHeight() / static_cast<float>(m_nrOfDuplicates);
+
+    noteBounds.setWidth(m_maxNoteWidth);
+    noteBounds.setHeight(m_maxNoteHeight);
 
     for (auto& note : m_notesVect)
     {
@@ -62,9 +66,6 @@ void NoteVisualiser::paint(juce::Graphics& g)
     g.setColour(m_backgroundClr.darker(.2f));
     g.fillRect(m_bounds);
 
-    float maxNoteWidth{ m_bounds.getWidth() / static_cast<float>(m_notesVect.size() / m_nrOfDuplicates) };
-    float maxNoteHeight{ m_bounds.getHeight() / static_cast<float>(m_nrOfDuplicates) };
-
     for (const auto& note : m_notesVect)
     {
         g.setColour((note->m_isActive ? note->m_colourActive : note->m_colourInactive));
@@ -72,14 +73,14 @@ void NoteVisualiser::paint(juce::Graphics& g)
         
         if (note->m_index == 0)
         {
-            juce::Line<float> line{ maxNoteWidth * note->m_noteNr + m_margin, 0.f,
-                                    maxNoteWidth * note->m_noteNr + m_margin, m_bounds.getHeight() + m_margin };
+            juce::Line<float> line{ m_maxNoteWidth * note->m_noteNr + m_margin, 0.f,
+                                    m_maxNoteWidth * note->m_noteNr + m_margin, m_bounds.getHeight() + m_margin };
             g.setColour(m_gradient.getColourAtPosition(note->m_noteNr / static_cast<float>(m_notesVect.size() / m_nrOfDuplicates)).brighter(.2f));
             g.drawLine(line, 1.f);
 
             g.drawFittedText(m_noteNamesVect[note->m_noteNr % 12],
-                             maxNoteWidth * note->m_noteNr + m_margin, m_margin,
-                             maxNoteWidth, maxNoteHeight,
+                             m_maxNoteWidth * note->m_noteNr + m_margin, m_margin,
+                             m_maxNoteWidth, m_maxNoteHeight,
                              juce::Justification::centred, 1);
         }
     }
@@ -93,10 +94,9 @@ void NoteVisualiser::UpdateNoteVisibility(GenerativeMelodicSequencerAudioProcess
 {
     int noteChanged{ p.GetActiveNote() };
     int noteIndex{ noteChanged * m_nrOfDuplicates };
-    float noteHeight{ m_bounds.getHeight() / static_cast<float>(m_nrOfDuplicates) * p.GetSequencerSettings(*p.GetAPVTS()).gate };
+    float noteHeight{ m_maxNoteHeight * p.GetSequencerSettings(*p.GetAPVTS()).gate };
     float noteDensity{ p.GetSequencerSettings(*p.GetAPVTS()).density };
     juce::Colour baseColour{m_gradient.getColourAtPosition(noteChanged / static_cast<float>(m_notesVect.size() / m_nrOfDuplicates))};
-
 
     if (!m_notesVect[noteIndex]->m_isActive)
     {
